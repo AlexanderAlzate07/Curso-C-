9477df644556cf163ebba99d18c4c19a402c71d6
AlexanderAlzate07 <alexander.alzatez@udea.edu.co>
2021-09-20 06:25:11 -0500

    Se agregó comentario en el Program.cs

---
+++ MyFirtsProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MySecondProgram;
+
+namespace MyFirtsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello world from C#";
+            float temperature = 20.0f;
+            Console.WriteLine($"{hello}");
+            Console.WriteLine("llamado a la función de conversión de temperatura:");
+            Console.WriteLine($"{temperature} grados Fahrenheit son {FahrenheitToCelsius(temperature)} grados celsius");
+            //se agrega comentario en el Program.cs
+            MyLibrary.PrimerMetodo();
+            MyLibrary.SegundoMetodo();
+            
+        }
+
+
+        static float FahrenheitToCelsius(float temperaureFahrenheit)
+        {
+            var temperaureCelsius = (temperaureFahrenheit - 32) / 1.8f;
+            return temperaureCelsius;
+        }
+    }
+}
---
+++ MyFirtsProgram/libreria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MySecondProgram
+{
+	public class MyLibrary
+	{
+		public static void PrimerMetodo() {
+			Console.WriteLine("este es el primer metodo de la libreria");
+		}
+
+		public static void SegundoMetodo()
+		{
+			Console.WriteLine("este es el segundo metodo de la libreria");
+		}
+
+		public static void TercerMetodo()
+		{
+			Console.WriteLine("prueba con la herramienta de git");
+		}
+	}
+}
